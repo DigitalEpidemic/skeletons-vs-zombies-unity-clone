bf995b5f674681633716edebe5bf6bc99133221a
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-05-21 14:12:24 -0400

    Added BetterCameraScript

---
+++ Assets/Scripts/BetterCameraScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterCameraScript : MonoBehaviour {
+
+    private Transform myTransform;
+    private Transform target;
+    public Vector3 offset = new Vector3 (0f, 0.6f, -0.5f);
+    private bool justStarted;
+    private float panSpeed = 2f;
+
+    void Awake () {
+        target = GameObject.Find ("Player").transform;
+        justStarted = true;
+        LockMouse ();
+    }
+
+    void Start () {
+        myTransform = this.transform;
+    }
+
+    void Update () {
+        // Follow character when spacebar is held
+        FollowPlayer ();
+        PanCamera ();
+    }
+
+    void FollowPlayer () {
+        if (Input.GetKey (KeyCode.Space)) {
+            if (target) {
+                myTransform.position = target.position + offset;
+                myTransform.LookAt (target.position, Vector3.up);
+
+            }
+        }
+
+        if (justStarted) {
+            justStarted = false;
+            if (target) {
+                myTransform.position = target.position + offset;
+                myTransform.LookAt (target.position, Vector3.up);
+
+            }
+        }
+    }
+
+    void PanCamera () {
+        // Pan with mouse
+        if (Input.mousePosition.x > Screen.width - 2f) {
+            Vector3 temp = transform.position;
+            temp.x += panSpeed * Time.deltaTime;
+
+            transform.position = temp;
+        }
+
+        if (Input.mousePosition.x < 1f) {
+            Vector3 temp = transform.position;
+            temp.x -= panSpeed * Time.deltaTime;
+
+            transform.position = temp;
+        }
+
+        if (Input.mousePosition.y > Screen.height - 2f) {
+            Vector3 temp = transform.position;
+            temp.z += panSpeed * Time.deltaTime;
+
+            transform.position = temp;
+        }
+
+        if (Input.mousePosition.y < 1f) {
+            Vector3 temp = transform.position;
+            temp.z -= panSpeed * Time.deltaTime;
+
+            transform.position = temp;
+        }
+    }
+
+    void LockMouse () {
+        // Confines mouse to game (Only in .exe)
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+} // BetterCameraScript
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour {
+
+    [SerializeField] private float shootDistance = 10f;
+    private float nextFire;
+    private float shootRate = 2f;
+
+    private Transform targetedEnemy;
+    bool enemyClicked;
+
+    bool running;
+    private Animator anim;
+    private NavMeshAgent navAgent;
+
+
+    void Start () {
+        anim = GetComponent<Animator> ();
+        navAgent = GetComponent<NavMeshAgent> ();
+    }
+
+    void Update () {
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetButtonDown ("Fire2")) {
+            // out sends the value back into the original variable
+            if (Physics.Raycast (ray, out hit, 100)) {
+                if (hit.collider.CompareTag ("Enemy")) {
+                    targetedEnemy = hit.transform;
+                    enemyClicked = true;
+
+                } else {
+                    running = true;
+                    enemyClicked = false;
+                    // Walk to point
+                    navAgent.destination = hit.point;
+                    navAgent.isStopped = false;
+                }
+            }
+        }
+
+        if (enemyClicked) {
+            MoveAndShoot ();
+        }
+
+        if (navAgent.remainingDistance <= navAgent.stoppingDistance) {
+            running = false;
+        } else {
+            running = true;
+        }
+
+        anim.SetBool ("isRunning", running);
+    }
+
+    void MoveAndShoot () {
+        if (targetedEnemy == null) {
+            return;
+        }
+
+        // Move towards enemy
+        navAgent.destination = targetedEnemy.position;
+        if (navAgent.remainingDistance >= shootDistance) {
+            navAgent.isStopped = false;
+            running = true;
+        }
+
+        if (navAgent.remainingDistance <= shootDistance) {
+            // Look at enemy
+            transform.LookAt (targetedEnemy);
+
+            // Sets time between attacks
+            if (Time.time > nextFire) {
+                nextFire = Time.time + shootRate;
+                Fire ();
+            }
+            navAgent.isStopped = true;
+            running = false;
+        }
+    }
+
+    void Fire () {
+        print ("FIRE!!");
+    }
+
+} // PlayerScript
