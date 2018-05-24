e3f0e9e1da41dd5c85b1f0560f3c6de7cdaf0811
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-05-23 20:26:13 -0400

    Added DestroyBullet script

---
+++ Assets/Scripts/DestroyBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBullet : MonoBehaviour {
+
+    public float timer = 3f;
+
+    void Start () {
+        Destroy (gameObject, timer);
+    }
+
+    void OnCollisionEnter (Collision target) {
+        Destroy (gameObject);
+    }
+
+} // DestroyBullet
